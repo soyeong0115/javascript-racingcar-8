cf21586154711b426bb0cb7f6904cfc625e61e9a
soyeong <mb535622@sookmyung.ac.kr>
2025-10-26 16:03:11 +0900

    feat: add round result output feature

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,9 +1,20 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const outputView = {
-    async printMessage(message) {
-        await Console.print(message);
-    }
+  async printRoundResult(cars) {
+    cars.forEach((car) => {
+      Console.print(`${car.name} : ${'-'.repeat(car.position)}`);
+    });
+    Console.print('');
+  },
+
+  async printWinners(winners) {
+    Console.print(`최종 우승자 : ${winners.join(', ')}`);
+  },
+
+  async printMessage(message) {
+    await Console.print(message);
+  },
 };
 
 export default outputView;
